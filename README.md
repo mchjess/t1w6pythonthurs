7ec9980358f666892b2d6441fc2e182803e87fe0
mchjess <15751@coderacademy.edu.au>
2024-06-07 16:02:38 +1000

    Additional changes made

--- README.md
+++ README.md
@@ -182,3 +182,20 @@ print(message)
 
 #### Match Case
 
+day_number = 3
+
+match day_number:
+    case 1:
+        day_name = "Monday"
+    case 2:
+        day_name = "Tuesday"
+    case 3:
+        day_name = "Wednesday"
+    case 4:
+        day_name = "Thursday"
+
+print(day_name)
+
+#### Activity 
+
+Write a python script
---
+++ activity.py
@@ -0,0 +1,16 @@
+## Prompts the user to enter a score.
+
+score = int(input("Enter your score (0-100): "))
+## Determines the grade based on the user input or score.
+if score >= 80:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"The grade for your score is {score} is {grade}") 
